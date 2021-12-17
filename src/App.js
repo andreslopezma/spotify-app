387ead61d21bc0e2be26fcff9a90d7af179c3dac
Andres <andres.lopezma@hotmail.com>
2021-12-17 00:04:52 -0500

    Archivo que renderiza los componente principales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import './index.css';
+import React, { Fragment, useState } from 'react';
+import NewRelase from './components/NewRealese';
+import HeaderNav from './components/HeaderNav';
+import Album from './components/Album';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  const [idAlbum, setIdAlbum] = useState('');
   return (
-    <h1>Prueba</h1>
+    <Fragment>
+      <HeaderNav />
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<NewRelase
+              setIdAlbum={setIdAlbum}
+            />} />
+            <Route path="/album" element={<Album
+              idAlbum={idAlbum}
+            />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </Fragment>
   );
 }
 
